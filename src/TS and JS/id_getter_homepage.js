d0afe7f8fb7e8d042140e75362e5d725cd40ee0b
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-07-03 14:10:37 -0300

    Updating Home Page Test
    I've just updated the document.

--- src/TS and JS/id_getter_homepage.js
+++ src/TS and JS/id_getter_homepage.js
@@ -1,6 +1,6 @@
 function id_getter_homepage(id) {
     if (id == 'home_page') {
-        let page1 = 'http://localhost:3000/'
+        let page1 = 'https://ademirramos.github.io/Routes/Home Page/'
         return page1
     }
 }
