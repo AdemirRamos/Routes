c4e823618b1912ff77be641a2da455576e69115b
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-07-03 14:11:39 -0300

    Updating Tailwind CSS Test
    I've just updated the document.

--- src/TS and JS/id_getter_tail.js
+++ src/TS and JS/id_getter_tail.js
@@ -1,6 +1,6 @@
 function id_getter_tail(id) {
     if (id == 'tailwind') {
-        let page3 = 'http://localhost:3000/tailwind/'
+        let page3 = 'https://ademirramos.github.io/Routes/Tailwind CSS/'
         return page3
     }
 }
