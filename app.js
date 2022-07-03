8bd945c087fa1b968ebd6ca28a85fc46dc8a8a00
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2022-07-03 12:47:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const routes = require('./Routes/routes/routes')
+const routes = require('./Routes/routes')
 const path = require('path')
 
 //CSS not working with Node JS: https://stackoverflow.com/questions/48248832/stylesheet-not-loaded-because-of-mime-type
